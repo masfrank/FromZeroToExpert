3ab999c7ec723326ad82792c2b8a9314a35b2394
masfrank <stinowork@outlook.it>
2022-11-21 15:54:13 +0800

    1-1 init Fromzerotoexpert

---
+++ src/test/java/com/spk/fromzerotoexpert/FromZerotoExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spk.fromzerotoexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromZerotoExpertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
